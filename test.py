c68658f922f971b7312756420781ed66186c98f7
wujishanxia <tianshibala@gmail.com>
2016-10-16 21:19:19 +0800

    successfully install pyqt 5.5

--- test.py
+++ test.py
@@ -1,1 +1,9 @@
-print "hello unittest"
+import sys
+from PyQt5 import QtCore, QtGui, QtWidgets
+app = QtWidgets.QApplication(sys.argv)
+w = QtWidgets.QWidget()
+w.resize(250, 150)
+w.move(300, 300)
+w.setWindowTitle('hello python unittest')
+w.show()
+sys.exit(app.exec_())
